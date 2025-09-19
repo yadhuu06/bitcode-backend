534487db0f52412f8f8e5982d95c994cacada551
Yadhu Krishnan PS <yadhuu.ps@gmail.com>
2025-09-19 15:45:02 +0530

    refactor :- added render port id to allowed host

--- bitWar_backend/settings.py
+++ bitWar_backend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']  
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','backend.bitcode.live','bitcode-backend.onrender.com']  
 
 
 INSTALLED_APPS = [
